51a4155a7bfe97aff6969c1c2042cd6af6795c3f
Alex <alexkristianmalau@gmail.com>
2023-05-10 15:28:12 +0700

    onboarding updated

--- app/src/main/java/com/bangkit/cloudraya/FragmentOnboarding.kt
+++ app/src/main/java/com/bangkit/cloudraya/FragmentOnboarding.kt
@@ -1,59 +1,52 @@
 package com.bangkit.cloudraya
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.bangkit.cloudraya.databinding.FirstOnboardBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentOnboarding.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FragmentOnboarding : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var  binding : FirstOnboardBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_onboarding, container, false)
+    ): View  {
+        binding = FirstOnboardBinding.inflate(inflater, container ,false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentOnboarding.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentOnboarding().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        transition()
     }
+
+    private fun transition(){
+        (activity as AppCompatActivity?)?.supportActionBar?.hide()
+        binding.btnStart.setOnClickListener {
+            binding.motionLayout.setTransition(R.id.toOnboard1)
+            binding.motionLayout.transitionToEnd()
+        }
+        binding.btnContinue2.setOnClickListener {
+            binding.motionLayout.setTransition(R.id.toOnboard2)
+            binding.motionLayout.transitionToEnd()
+        }
+        binding.btnContinue3.setOnClickListener {
+            binding.motionLayout.setTransition(R.id.toOnboard3)
+            binding.motionLayout.transitionToEnd()
+        }
+        binding.btnContinue4.setOnClickListener {
+            binding.motionLayout.setTransition(R.id.toOnboard4)
+            binding.motionLayout.transitionToEnd()
+        }
+//        binding.btnContinue5.setOnClickListener {
+//            binding.motionLayout.setTransition(R.id.to)
+//            binding.motionLayout.transitionToEnd()
+//        }
+    }
+
 }
