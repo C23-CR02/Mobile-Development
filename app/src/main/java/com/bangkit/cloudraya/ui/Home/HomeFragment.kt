c6519675e66603731baaa2147d1d0945bbea5baa
Alex <alexkristianmalau@gmail.com>
2023-05-12 02:57:43 +0700

    Updated

--- app/src/main/java/com/bangkit/cloudraya/ui/Home/HomeFragment.kt
+++ app/src/main/java/com/bangkit/cloudraya/ui/Home/HomeFragment.kt
@@ -1,16 +1,17 @@
 package com.bangkit.cloudraya.ui.Home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.bangkit.cloudraya.databinding.FragmentHomeBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : Fragment() {
-    private lateinit var binding : FragmentHomeBinding
+    private lateinit var binding: FragmentHomeBinding
     private val viewModel: HomeViewModel by viewModel()
 
     override fun onCreateView(
@@ -24,6 +25,7 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.btnResources.setOnClickListener {
+            Log.d("Testing","1")
             val toResources = HomeFragmentDirections.actionHomeFragmentToFragmentResources()
             findNavController().navigate(toResources)
         }
