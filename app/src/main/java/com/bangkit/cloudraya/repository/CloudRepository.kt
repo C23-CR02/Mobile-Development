995907ef5002d1f437971e7e74d34463c3f3f7df
Daffa <daffastef14n@gmail.com>
2023-05-11 14:23:32 +0700

    add detail layout and hit api

--- app/src/main/java/com/bangkit/cloudraya/repository/CloudRepository.kt
+++ app/src/main/java/com/bangkit/cloudraya/repository/CloudRepository.kt
@@ -1,8 +1,36 @@
 package com.bangkit.cloudraya.repository
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
+import com.bangkit.cloudraya.model.local.Event
+import com.bangkit.cloudraya.model.remote.VMListResponse
 import com.bangkit.cloudraya.network.ApiService
+import kotlinx.coroutines.Dispatchers
+import org.json.JSONObject
 
 class CloudRepository(private val apiService: ApiService) {
 
+    fun getVMList(): LiveData<Event<VMListResponse>> =
+        liveData(Dispatchers.IO){
+            emit(Event.Loading)
+            try {
+                val response = apiService.getVMList("Bearer ")
+                if (response.isSuccessful){
+                    val data = response.body()
+                    data?.let {
+                        emit(Event.Success(it))
+                    }
+                }else{
+                    val error = response.errorBody()?.toString()
+                    if (error != null){
+                        val jsonObject = JSONObject(error)
+                        val message = jsonObject.getString("message")
+                        emit(Event.Error(null, message))
+                    }
+                }
+            }catch (e: Exception){
+                emit(Event.Error(null, e.toString()))
+            }
+        }
 
 }
--- app/src/main/java/com/bangkit/cloudraya/ui/DetailVM/DetailVMViewModel.kt
+++ app/src/main/java/com/bangkit/cloudraya/ui/DetailVM/DetailVMViewModel.kt
@@ -4,4 +4,5 @@ import androidx.lifecycle.ViewModel
 import com.bangkit.cloudraya.repository.CloudRepository
 
 class DetailVMViewModel(private val cloudRepository: CloudRepository): ViewModel() {
+    fun getVMList() = cloudRepository.getVMList()
 }
