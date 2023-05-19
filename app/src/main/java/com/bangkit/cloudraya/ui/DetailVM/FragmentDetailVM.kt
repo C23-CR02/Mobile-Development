2b62ef3c7a8133ac94ed07f654ac830b44387ae2
Daffa <daffastef14n@gmail.com>
2023-05-19 13:55:26 +0700

    menyesuaikan detail dan token

--- app/src/main/java/com/bangkit/cloudraya/ui/DetailVM/FragmentDetailVM.kt
+++ app/src/main/java/com/bangkit/cloudraya/ui/DetailVM/FragmentDetailVM.kt
@@ -20,6 +20,7 @@ class FragmentDetailVM : Fragment() {
     private lateinit var binding: FragmentDetailVmBinding
     private val viewModel: DetailVMViewModel by viewModel()
     private lateinit var vmData: VMListData
+    private lateinit var site: String
     private var token = ""
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,6 +33,9 @@ class FragmentDetailVM : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         vmData = FragmentDetailVMArgs.fromBundle(arguments as Bundle).vmData
+        site = FragmentDetailVMArgs.fromBundle(arguments as Bundle).siteName
+        val data = viewModel.getListEncrypted(token)
+        token = data[2].toString()
         observeData(vmData)
         binding.btnBack.setOnClickListener {
             findNavController().popBackStack()
@@ -49,15 +53,72 @@ class FragmentDetailVM : Fragment() {
 
     private fun observeData(vmData: VMListData) {
         Log.d("Request Detail", vmData.localId.toString())
-        lifecycleScope.launch {
-            token = viewModel.getToken()[0].token
-            viewModel.getVMDetail(token, vmData.localId!!).observe(viewLifecycleOwner) { result ->
+        viewModel.getVMDetail(token, vmData.localId!!).observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is Event.Success -> {
+                    updateUI(result.data.data!!)
+                }
+                is Event.Error -> {
+                    Snackbar.make(binding.root, result.error ?: "Error", Snackbar.LENGTH_SHORT)
+                        .show()
+                }
+                is Event.Loading -> {
+
+                }
+            }
+        }
+//        lifecycleScope.launch {
+//            token = viewModel.getToken()[0].token
+//        }
+    }
+
+
+
+    private fun startVM() {
+        viewModel.vmAction(token, vmData.localId!!, "start")
+            .observe(viewLifecycleOwner) { result ->
                 when (result) {
                     is Event.Success -> {
-                        updateUI(result.data.data!!)
+                        Snackbar.make(
+                            binding.root,
+                            "${result.data.data} - ${result.data.message}",
+                            Snackbar.LENGTH_LONG
+                        ).show()
                     }
                     is Event.Error -> {
-                        Snackbar.make(binding.root, result.error ?: "Error", Snackbar.LENGTH_SHORT)
+                        Snackbar.make(
+                            binding.root,
+                            result.error ?: "Error",
+                            Snackbar.LENGTH_SHORT
+                        ).show()
+                    }
+                    is Event.Loading -> {
+
+                    }
+                }
+            }
+//        lifecycleScope.launch {
+//            token = viewModel.getToken()[0].token
+//        }
+    }
+
+    private fun stopVM() {
+        viewModel.vmAction(token, vmData.localId!!, "stop")
+            .observe(viewLifecycleOwner) { result ->
+                when (result) {
+                    is Event.Success -> {
+                        Snackbar.make(
+                            binding.root,
+                            "${result.data.data} - ${result.data.message}",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                    is Event.Error -> {
+                        Snackbar.make(
+                            binding.root,
+                            result.error ?: "Error",
+                            Snackbar.LENGTH_SHORT
+                        )
                             .show()
                     }
                     is Event.Loading -> {
@@ -65,105 +126,50 @@ class FragmentDetailVM : Fragment() {
                     }
                 }
             }
-        }
-    }
-
-
-
-    private fun startVM() {
-        lifecycleScope.launch {
-            token = viewModel.getToken()[0].token
-            viewModel.vmAction(token, vmData.localId!!, "start")
-                .observe(viewLifecycleOwner) { result ->
-                    when (result) {
-                        is Event.Success -> {
-                            Snackbar.make(
-                                binding.root,
-                                "${result.data.data} - ${result.data.message}",
-                                Snackbar.LENGTH_LONG
-                            ).show()
-                        }
-                        is Event.Error -> {
-                            Snackbar.make(
-                                binding.root,
-                                result.error ?: "Error",
-                                Snackbar.LENGTH_SHORT
-                            ).show()
-                        }
-                        is Event.Loading -> {
-
-                        }
-                    }
-                }
-        }
-    }
-
-    private fun stopVM() {
-        lifecycleScope.launch {
-            token = viewModel.getToken()[0].token
-            viewModel.vmAction(token, vmData.localId!!, "stop")
-                .observe(viewLifecycleOwner) { result ->
-                    when (result) {
-                        is Event.Success -> {
-                            Snackbar.make(
-                                binding.root,
-                                "${result.data.data} - ${result.data.message}",
-                                Snackbar.LENGTH_LONG
-                            ).show()
-                        }
-                        is Event.Error -> {
-                            Snackbar.make(
-                                binding.root,
-                                result.error ?: "Error",
-                                Snackbar.LENGTH_SHORT
-                            )
-                                .show()
-                        }
-                        is Event.Loading -> {
-
-                        }
-                    }
-                }
-        }
+//        lifecycleScope.launch {
+//            token = viewModel.getToken()[0].token
+//        }
     }
 
     private fun restartVM() {
-        lifecycleScope.launch {
-            token = viewModel.getToken()[0].token
-            Log.d("Testing", token)
-            viewModel.vmAction(token, vmData.localId!!, "reboot")
-                .observe(viewLifecycleOwner) { result ->
-                    when (result) {
-                        is Event.Success -> {
-                            Snackbar.make(
-                                binding.root,
-                                "${result.data.data} - ${result.data.message}",
-                                Snackbar.LENGTH_LONG
-                            ).show()
-                        }
-                        is Event.Error -> {
-                            Snackbar.make(
-                                binding.root,
-                                result.error ?: "Error",
-                                Snackbar.LENGTH_SHORT
-                            )
-                                .show()
-                        }
-                        is Event.Loading -> {
+        viewModel.vmAction(token, vmData.localId!!, "reboot")
+            .observe(viewLifecycleOwner) { result ->
+                when (result) {
+                    is Event.Success -> {
+                        Snackbar.make(
+                            binding.root,
+                            "${result.data.data} - ${result.data.message}",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                    is Event.Error -> {
+                        Snackbar.make(
+                            binding.root,
+                            result.error ?: "Error",
+                            Snackbar.LENGTH_SHORT
+                        )
+                            .show()
+                    }
+                    is Event.Loading -> {
 
-                        }
                     }
                 }
-        }
+            }
+//        lifecycleScope.launch {
+//            token = viewModel.getToken()[0].token
+//            Log.d("Testing", token)
+//        }
     }
 
     private fun updateUI(data: VMData) {
-        binding.tvName.text = data.hostname
-        binding.tvLocation.text = data.location
-        binding.tvImage.text = data.os
-        binding.tvIPAddress.text = data.publicIp
-        binding.tvLaunchDate.text = data.launchDate
-        binding.tvStatus.text = data.state
+        binding.apply {
+            tvName.text = data.hostname
+            tvLocation.text = data.location
+            tvImage.text = data.os
+            tvIPAddress.text = data.publicIp
+            tvLaunchDate.text = data.launchDate
+            tvStatus.text = data.state
+        }
 
     }
 }
